2050120e5acfdba23a814ee82c3a5a513ea3bb56
Srihari <Srhrsrihari@gmail.com>
2020-05-24 14:44:01 +0530

    container fix, bg and font changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,23 +3,24 @@ let dataUrl =
   "http://newsapi.org/v2/top-headlines?country=in&apiKey=c085b3037a4d489b9887bdf45c143313";
 
 $(document).ready(function () {
-  $.get(proxyUrl + dataUrl, function (data) {
+  $.get( dataUrl, function (data) {
     let totalArticles = data.articles;
     totalArticles.forEach((article) => {
       if (article.content!=null){
-      truncArticlecontent = article.content.split(" ").splice(0,25).join(" ");
+      truncArticlecontent = article.content.split(" ").splice(0,20).join(" ");
       }
-      else {truncArticlecontent = article.description}
-      $(".js-news-container").append(
-        `<div class="card" style="width:18rem;">
-            <img class="card-img-top" src="${article.urlToImage}" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">${article.title}</h5>
-              <p class="card-text">${truncArticlecontent}...</p>
-              <a href="${article.url}" class="btn btn-primary">Read more</a>
-            </div>
-          </div>
-          <hr class="my-4">`
+      else {truncArticlecontent = article.description.split(" ").splice(0,20).join(" ")}
+      $(".js-headlines-container").append(
+        `<div class="news-box">
+        <div>
+            <img class="news-img" src="${article.urlToImage}" alt="Card image cap">
+            <div class="news-body">
+              <h5 class="news-title">${article.title}</h5>
+              <p class="news-content">${truncArticlecontent}...</p>
+              </div>
+              </div>
+              <a href="${article.url}" class="btn">Read more</a>
+          </div>`
       );
     });
   });
