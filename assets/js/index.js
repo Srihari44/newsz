59fb5f3643200aab019703a66f7db7a13bbc823b
Srihari <Srhrsrihari@gmail.com>
2020-05-24 19:40:09 +0530

    More containers add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,37 @@
 let proxyUrl = "https://cors-anywhere.herokuapp.com/";
-let dataUrl =
-  "http://newsapi.org/v2/top-headlines?country=in&apiKey=c085b3037a4d489b9887bdf45c143313";
+let dataUrl = "http://newsapi.org/v2/top-headlines?country=in";
+const apiKey = "&apiKey=c085b3037a4d489b9887bdf45c143313"
+  
+  $(document).on({
+    ajaxStop: function() {  $(".spinner-border").hide(); }    
+});
 
 $(document).ready(function () {
-  $.get(proxyUrl+dataUrl, function (data) {
+  getData("","headlines")
+  getData("&category=business","business")
+  getData("&category=entertainment","entertainment")
+  getData("&category=health","health")
+  getData("&category=science","science")
+  getData("&category=sports","sports")
+  getData("&category=technology","technology")
+});
+
+let getData = (query, conName) => {
+  let queryUrl = proxyUrl + dataUrl + query + apiKey
+  let conClass = ".js-" + conName + "-container"
+  $.get(queryUrl, function (data) {
     let totalArticles = data.articles;
     totalArticles.forEach((article) => {
       if (article.content!=null){
       truncArticlecontent = article.content.split(" ").splice(0,20).join(" ");
       }
-      else {truncArticlecontent = article.description.split(" ").splice(0,20).join(" ")}
-      $(".js-headlines-container").append(
+      else {
+        if (article.description!=null){
+        truncArticlecontent = article.description.split(" ").splice(0,20).join(" ")
+      }
+      else {truncArticlecontent = ""}
+    }
+      $(conClass).append(
         `<div class="news-box">
         <div>
             <img class="news-img" src="${article.urlToImage}" alt="Card image cap">
@@ -23,5 +44,13 @@ $(document).ready(function () {
           </div>`
       );
     });
-  });
-});
+  }).then(()=> {
+    console.log("Done "+conName)
+  })
+  };
+
+
+let searcharticles = () =>{
+  const inpValue = $('#queryInp').value;
+
+}
