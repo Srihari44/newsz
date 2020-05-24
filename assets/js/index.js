b523cc4a755b2818685e297b01bd9b688b13dbf6
Srihari <Srhrsrihari@gmail.com>
2020-05-25 00:45:00 +0530

    footer fix, search func add

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,13 +7,13 @@ const apiKey = "&apiKey=c085b3037a4d489b9887bdf45c143313"
 });
 
 $(document).ready(function () {
-  getData("","headlines")
-  getData("&category=business","business")
-  getData("&category=entertainment","entertainment")
-  getData("&category=health","health")
-  getData("&category=science","science")
-  getData("&category=sports","sports")
-  getData("&category=technology","technology")
+  // getData("","headlines")
+  // getData("&category=business","business")
+  // getData("&category=entertainment","entertainment")
+  // getData("&category=health","health")
+  // getData("&category=science","science")
+  // getData("&category=sports","sports")
+  // getData("&category=technology","technology")
 });
 
 let getData = (query, conName) => {
@@ -51,6 +51,30 @@ let getData = (query, conName) => {
 
 
 let searcharticles = () =>{
-  const inpValue = $('#queryInp').value;
 
+  const inpValue = document.getElementById('queryInp').value;
+  let searchUrl = proxyUrl + "https://newsapi.org/v2/everything?q=" + inpValue + "&page=1" + apiKey
+  $('#searchModal').modal('show')
+  $('.js-Search-con').empty();
+  document.getElementById('searchTitle').innerText = "Search results for " + inpValue 
+  $('.js-Search-con').empty();
+  $.get(searchUrl, (results) => {
+    let totalArticles = results.articles;
+    totalArticles.forEach((article)=>{
+      let truncArticlecontent = article.description.split(" ").splice(0,20).join(" ")
+      $('.js-Search-con').append(
+        ` <a href="${article.url}" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">${article.title}</h5>
+        <small>${article.publishedAt.slice(0,10)}</small>
+        </div>
+        <p class="mb-1">${truncArticlecontent}</p>
+      <small>${article.source.name}</small>
+      </a>`
+      )
+    })
+
+  })
 }
+
+window.addEventListener("hashchange", function() { scrollBy(0, -50) })
