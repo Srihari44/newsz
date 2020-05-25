f43b649b37ecbb87865024e9546aebb8753eafdb
Srihari <Srhrsrihari@gmail.com>
2020-05-25 20:25:32 +0530

    article Modal add

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,13 @@ let proxyUrl = "https://cors-anywhere.herokuapp.com/";
 let dataUrl = "http://newsapi.org/v2/top-headlines?country=in";
 const apiKey = "&apiKey=c085b3037a4d489b9887bdf45c143313";
 
+
 $(document).on({
   ajaxStop: function () {
     $(".spinner-border").hide();
-  },
+  }
 });
+
 
 $(document).ready(function () {
   getData("", "headlines");
@@ -18,8 +20,9 @@ $(document).ready(function () {
   getData("&category=technology", "technology");
 });
 
+
 let getData = (query, conName) => {
-  let queryUrl = proxyUrl + dataUrl + query + apiKey;
+  let queryUrl = proxyUrl+dataUrl + query + apiKey;
   let conClass = ".js-" + conName + "-container";
   $.get(queryUrl, function (data) {
     let totalArticles = data.articles;
@@ -42,13 +45,13 @@ let getData = (query, conName) => {
       $(conClass).append(
         `<div class="news-box">
         <div>
-            <img class="news-img" src="${article.urlToImage}" alt="Card image cap">
+            <img class="news-img" src="${article.urlToImage}" alt="${article.title}">
             <div class="news-body">
               <h5 class="news-title">${article.title}</h5>
               <p class="news-content">${truncArticlecontent}...</p>
               </div>
               </div>
-              <a href="${article.url}" class="btn">Read more</a>
+              <button class="btn" onclick="openArticle(["${article.title}","${article.content}","${article.author}","${article.publishedAt}","${article.url}","${article.urlToImage}","${article.source.name}"])">Read more</a>
           </div>`
       );
     });
@@ -56,6 +59,27 @@ let getData = (query, conName) => {
     console.log("Done " + conName);
   });
 };
+
+
+let openArticle = (articleArr) => {
+  $("#articleModal").modal("show");
+  $(".js-Article-con").empty();
+  document.getElementById("articleTitle").innerText = articleArr[0];
+    $(".js-Article-con").append(
+      `<div class="d-flex w-100 justify-content-center">
+      <small>${articleArr[2]}</small>
+        <small>${articleArr[3]}</small>
+        </div>
+        <img class="news-img" src="${articleArr[5]}" alt="${articleArr[0]}">
+        <p class="mb-1">${articleArr[1]}</p>
+      <small>${articleArr[6]}</small>
+      `
+    );
+    $(".js-article-footer").append(
+      `<button type="button" class="btn" onclick="location.href = '${articleArr[4]}'">Visit source</button>`
+    )
+}
+
 
 let searcharticles = () => {
   const inpValue = document.getElementById("queryInp").value;
@@ -69,7 +93,6 @@ let searcharticles = () => {
   $(".js-Search-con").empty();
   document.getElementById("searchTitle").innerText =
     "Search results for " + inpValue;
-  $(".js-Search-con").empty();
   $.get(searchUrl, (results) => {
     let totalArticles = results.articles;
     totalArticles.forEach((article) => {
@@ -92,6 +115,7 @@ let searcharticles = () => {
     });
   });
 };
+
 
 window.addEventListener("hashchange", function () {
   scrollBy(0, -50);
