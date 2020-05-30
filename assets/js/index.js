6639ce73b3007007d8266852372ab1a10c09df00
Srihari <Srhrsrihari@gmail.com>
2020-05-30 23:01:25 +0530

    proxychange-2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-let proxyUrl = "https://crossorigin.me/";
-let dataUrl = "http://newsapi.org/v2/top-headlines?country=in";
+let proxyUrl = "https://cors-anywhere.herokuapp.com/";
+let dataUrl = "https://newsapi.org/v2/top-headlines?country=in";
 const apiKey = "&apiKey=c085b3037a4d489b9887bdf45c143313";
 
 $(document).on({
